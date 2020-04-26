4d09df6931a07d463f42b8cf1bc5ed5f83dc390a
alifdelgado <alifdelgado@gmail.com>
2020-04-26 04:34:30 -0500

    Sección sobre los PIPES de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'cApItAn AmÉrIcA';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  PI: number = Math.PI;
+  porcentaje = 0.234;
+  salario = 1234.5;
+  fecha: Date = new Date();
+  idioma = 'es';
+  video = 'https://www.youtube.com/embed/yaI6-uI0XrQ';
+  valorPromesa = new Promise((resolve) => {
+    setTimeout(() => {
+    resolve('llego la data');
+    }, 4500);
+  });
+  heroe = {
+    nombre: 'Logan',
+    clave: 'wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  };
 }
